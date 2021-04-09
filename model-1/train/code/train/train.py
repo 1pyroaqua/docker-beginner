11ce8882f259c06dac71f609682cbf54ec00f1a4
1pyroaqua <1pyroaqua@gmail.com>
2021-04-09 02:08:55 +0000

    Initial Commit of required files

---
+++ model-1/train/code/train/train.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import os
+import pandas as pd
+from sklearn import linear_model
+import pickle
+
+# training and saving some model
+reg_model = linear_model.LinearRegression()
+reg_model.fit([[2.,2.,5.], [1.,1.,5.], [3.,3.,2.]], [0.,0.,1.])
+pickle.dump(reg_model, open('model.pkl', 'wb'))
+print("Model training completed")
+
